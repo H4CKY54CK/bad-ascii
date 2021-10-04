e94a51e5c9eaffd98d39a04362ee1804829c1051
H4CKY54CK <tk13xr37@gmail.com>
2021-10-04 05:19:51 -0400

    corrections/improvements

--- badascii.py
+++ badascii.py
@@ -2,20 +2,18 @@ from PIL import Image
 import cv2
 import time
 
+
 def convert(img, size=(50,50)):
     chars = ' .,:;+*%#@'
-    # chars = chars[::-1]
-    scale = {c:i for c,i in zip(chars, range(0,len(chars) * 10,10))}
     img = Image.fromarray(img)
     img.thumbnail(size)
     data = [[] for _ in range(img.height)]
     for x in range(img.width):
         for y in range(img.height):
             avg = img.getpixel((x,y))[0]
-            avg /= 2.55
+            avg //= 2.55
             for i in range(0,len(chars) * 10,10):
-                if avg < i:
-                    i -= 1
+                if avg <= i:
                     break
             i //= 10
             char = chars[i]
